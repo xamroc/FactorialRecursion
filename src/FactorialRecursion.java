f941c28b4b0aaaf8c2ff32ff38b21bb6d71033c9
Marco Lau <lau.career@gmail.com>
2013-08-14 15:09:31 +0800

    Preliminary setup

---
+++ src/FactorialRecursion.java
@@ -0,0 +1,18 @@
+
+public class FactorialRecursion {
+
+	public static void main(String[] args) {
+
+		int n = 5;
+
+		System.out.println(factorial(n));
+
+	}
+
+	public static int factorial(int n) {
+		
+		if(n==0) return 1;
+		return 1;
+	}
+
+}
