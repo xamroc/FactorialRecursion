0cc7b3133636d96ec4ca02d9c2e2b0022194abfc
Marco Lau <lau.career@gmail.com>
2013-08-14 15:29:28 +0800

    Complete Recursive Factorial

--- src/FactorialRecursion.java
+++ src/FactorialRecursion.java
@@ -4,7 +4,13 @@ public class FactorialRecursion {
 	public static void main(String[] args) {
 
 		int n = 5;
-
+		
+		/** Setup for user input at later iteration
+		while(n<0) {
+			System.out.println("Number should be positive!");
+		}
+		*/
+		
 		System.out.println(factorial(n));
 
 	}
@@ -12,7 +18,11 @@ public class FactorialRecursion {
 	public static int factorial(int n) {
 		
 		if(n==0) return 1;
-		return 1;
+		
+		else if(n>=0) {
+			return (n * factorial(n-1));
+		}
+		
+		return 0;
 	}
-
 }
